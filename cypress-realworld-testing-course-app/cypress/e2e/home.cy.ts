2797e282aa086d7f05397999ec28cedbb315fa43
royalbrawler <>
2023-07-15 16:59:17 +0300

    add tests, typo fix, organizing with context

--- cypress-realworld-testing-course-app/cypress/e2e/home.cy.ts
+++ cypress-realworld-testing-course-app/cypress/e2e/home.cy.ts
@@ -1,23 +1,42 @@
-describe("hame page", () => {
+describe("home page", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000")
     cy.get("h1", { log: false }).should("be.visible")
   })
 
-  it("the h1 contains the correct text", () => {
-    cy.get(`[data-test="hero-heading"]`).contains(
-      "Testing Next.js Applications with Cypress"
-    )
+  context("Hero section", () => {
+    it("the h1 contains the correct text", () => {
+      // cy.get(`[data-test="hero-heading"]`).contains(
+      //   "Testing Next.js Applications with Cypress"
+      // )
 
-    // custom command
-    cy.getByData("hero-heading").contains(
-      "Testing Next.js Applications with Cypress"
-    )
+      // custom command
+      cy.getByData("hero-heading").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
+
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+
+    it("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
   })
 })
