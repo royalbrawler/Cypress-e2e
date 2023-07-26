0cc9b9c13015f7c19d5fc11824ffefda880fb777
royalbrawler <>
2023-07-26 18:21:57 +0300

    fake data demo

---
+++ cypress-learning/cypress/e2e/36-faker.cy.js
@@ -0,0 +1,22 @@
+describe('Docket Post Test', () => {
+    it('Random User', () => {
+        cy.task('freshUser').then(obj => {
+            cy.log(obj)
+        })
+    })
+    it('Random User', () => {
+        cy.task('freshUser').then(obj => {
+            cy.log(obj)
+        })
+    })
+    it('Random User', () => {
+        cy.task('freshUser').then(obj => {
+            cy.log(obj)
+        })
+    })
+    it('Random User', () => {
+        cy.task('freshUser').then(obj => {
+            cy.log(obj)
+        })
+    })
+})
